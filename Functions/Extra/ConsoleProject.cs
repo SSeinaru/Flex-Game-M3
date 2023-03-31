a8bb49f6c12ba062f9c0cbedbebcd16654aea87c
Seinaru <34807@ma-web.nl>
2023-03-31 09:52:02 +0200

    Al gemaakte property opdrachten

---
+++ Functions/Extra/ConsoleProject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleProject
+public class ConsoleProject
+{
+	internal class Project()
+	{
+		public void Arguments(int, string)
+		{
+			Console.WriteLine(int)
+			Console.WriteLine(string)
+		}
+	Arguments(8543, "Somebody once told me");
+		
+}
---
+++ Properties/PropertyOpdracht1/PropertyOpdracht1/Persoon.cs
@@ -0,0 +1,13 @@
+﻿namespace PropertyOpdracht1
+{
+    internal class Persoon
+    {
+        public Persoon()
+        {
+           
+        }
+
+        public string Naam { get; internal set; }
+        public int Leeftijd { get; internal set; }
+    }
+}
